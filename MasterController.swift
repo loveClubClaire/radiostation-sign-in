9a289ed5aaae13b8bcae91c9d71a2c57a92a3af0
Zach <zachary.whitten.11@cnu.edu>
2016-04-08 23:49:43 -0400

    Created MasterController Swift class and binded the text fields and buttons to the class

---
+++ MasterController.swift
@@ -0,0 +1,21 @@
+//
+//  MasterController.swift
+//  WCNU Station Sign In
+//
+//  Created by Zachary Whitten on 4/8/16.
+//  Copyright © 2016 WCNURadio. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class MasterController: NSObject{
+    @IBOutlet weak var showNameTextField: NSTextFieldCell!
+    @IBOutlet weak var emailOneTextField: NSTextFieldCell!
+    @IBOutlet weak var emailTwoTextField: NSTextFieldCell!
+    @IBOutlet weak var stationStatusTextField: NSTextFieldCell!
+    @IBOutlet weak var podcastNameTextField: NSTextFieldCell!
+    @IBOutlet weak var isPodcastButton: NSButton!
+    
+    
+}
