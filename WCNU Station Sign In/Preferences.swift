91c3dff7567b3cf02c9520ca708cbac09b2d383b
Zach <zachary.whitten.11@cnu.edu>
2016-04-09 00:47:41 -0400

    Created preferences windows in IB and created a preferences class

---
+++ WCNU Station Sign In/Preferences.swift
@@ -0,0 +1,17 @@
+//
+//  Preferences.swift
+//  WCNU Station Sign In
+//
+//  Created by Zachary Whitten on 4/9/16.
+//  Copyright © 2016 WCNURadio. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class Preferences: NSObject {
+    
+    @IBOutlet weak var generalPreferences: NSToolbarItem!
+    @IBOutlet weak var podcastsPreferences: NSToolbarItem!
+    
+}
