3fd02f2e103ffd76c1fdf29a7691b6914da029f8
Zach <zachary.whitten.11@cnu.edu>
2016-04-09 15:42:17 -0400

    Created fileSelectors for the other three parts of the preference windows with variables to store the received data.

--- WCNU Station Sign In/Preferences.swift
+++ WCNU Station Sign In/Preferences.swift
@@ -14,8 +14,30 @@ class Preferences: NSObject {
     @IBOutlet weak var podcastsPreferencesView: NSView!
     @IBOutlet weak var generalPreferencesView: NSView!
     @IBOutlet weak var signinMenu: NSMenu!
+    @IBOutlet weak var podcastDestinationMenu: NSMenu!
+    @IBOutlet weak var songLogMenu: NSMenu!
+    @IBOutlet weak var podcastLocationMenu: NSMenu!
+    
     var signinFilepath = ""
-   
+    var podcastFilepath = ""
+    var songLogFilepath = ""
+    var podcastDestinationFilepath = ""
+    
+
+    override init() {
+        var fileManager
+    }
+    
+//    //Load saved preference data
+//    NSFileManager *fileManager = [[NSFileManager alloc]init];
+//    NSURL *filepath = [[NSURL alloc]init];
+//    filepath = [fileManager URLForDirectory:NSApplicationSupportDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+//    NSString *stringFilepath = [filepath path];
+//    NSString *savedDataFilepath = [NSString stringWithFormat:@"%@/WCNURadio Automated Secretary",stringFilepath];
+//    if ([fileManager fileExistsAtPath:savedDataFilepath] == false) {
+//    [fileManager createDirectoryAtPath:savedDataFilepath withIntermediateDirectories:NO attributes:nil error:nil];
+//    }
+//    _preferencesFilePath = [savedDataFilepath stringByAppendingPathComponent:@"preferences.txt"];
 
     
     //place the generalPreferencesView onto the generalPreferenceWindow and change the size of the window to fit the frame. Change the title of the window as well
@@ -56,6 +78,59 @@ class Preferences: NSObject {
         else{
             signinMenu.performActionForItemAtIndex(0)
         }
+    }
+    
+    //When the user requests to change the location of the podcast location folder this function is called. It spawns a file selector panel which can only select a single directory. If the user selects a directory, setFolderMenu is called to update the menu item so that it shows the new directory.
+    @IBAction func getPodcastLocationFolder(sender: AnyObject) {
+        let myPanel = NSOpenPanel()
+        myPanel.allowsMultipleSelection = false
+        myPanel.canChooseDirectories = true
+        myPanel.canChooseFiles = false;
+        //If user confirms then call setFolderMenu to update the menu object and update the filepath by setting the podcastFilepath variable
+        if myPanel.runModal() == NSModalResponseOK {
+            setFolderMenu(myPanel.URLs[0].path!, aMenu: podcastLocationMenu)
+            podcastFilepath = myPanel.URLs[0].path!
+        }
+            //If the user cancels then make the menu have the currect directory selected, not the change filepath option selected. Its a UI thing.
+        else{
+            podcastLocationMenu.performActionForItemAtIndex(0)
+        }
+
+    }
+    
+    //When the user requests to change the location of the song log folder this function is called. It spawns a file selector panel which can only select a single directory. If the user selects a directory, setFolderMenu is called to update the menu item so that it shows the new directory.
+    @IBAction func getSongLogFolder(sender: AnyObject) {
+        let myPanel = NSOpenPanel()
+        myPanel.allowsMultipleSelection = false
+        myPanel.canChooseDirectories = true
+        myPanel.canChooseFiles = false;
+        //If user confirms then call songLogMenu to update the menu object and update the filepath by setting the songLogFilepath variable
+        if myPanel.runModal() == NSModalResponseOK {
+            setFolderMenu(myPanel.URLs[0].path!, aMenu: songLogMenu)
+            songLogFilepath = myPanel.URLs[0].path!
+        }
+            //If the user cancels then make the menu have the currect directory selected, not the change filepath option selected. Its a UI thing.
+        else{
+            songLogMenu.performActionForItemAtIndex(0)
+        }
+    }
+    
+    //When the user requests to change the location of the podcast destination folder this function is called. It spawns a file selector panel which can only select a single directory. If the user selects a directory, setFolderMenu is called to update the menu item so that it shows the new directory.
+    @IBAction func getPodcastDestinationFolder(sender: AnyObject) {
+        let myPanel = NSOpenPanel()
+        myPanel.allowsMultipleSelection = false
+        myPanel.canChooseDirectories = true
+        myPanel.canChooseFiles = false;
+        //If user confirms then call podcastDestinationMenu to update the menu object and update the filepath by setting the podcastDestinationFilepath variable
+        if myPanel.runModal() == NSModalResponseOK {
+            setFolderMenu(myPanel.URLs[0].path!, aMenu: podcastDestinationMenu)
+            podcastDestinationFilepath = myPanel.URLs[0].path!
+        }
+            //If the user cancels then make the menu have the currect directory selected, not the change filepath option selected. Its a UI thing.
+        else{
+            podcastDestinationMenu.performActionForItemAtIndex(0)
+        }
+
     }
     
     //Gets a filepath and a menu and sets the first menuItem in the menu to the file given by the filepath and then selects that menuItem
